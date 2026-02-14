218cf731b07db8df879f5eb72d9e04b4bfb736cf
Manu <manuange@example.com>
2026-02-14 11:26:52 +0100

    Login & register forms styles

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -73,4 +73,69 @@ const LoginForm = () => {
   );
 };
 
+const styles = {
+  container: {
+    display: 'flex',
+    minHeight: '100vh',
+    fontFamily: 'Arial, sans-serif',
+  },
+  right: {
+    flex: 1,
+    backgroundColor: '#b0dffb',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '150px',
+    borderRadius: '0 20px 20px 0',
+    boxShadow: '-4px 0 15px rgba(215, 215, 215, 0.1)',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '15px',
+    width: '100%',
+    maxWidth: '350px',
+  },
+  input: {
+    padding: '12px',
+    borderRadius: '8px',
+    border: '1px solid #CBD5E1',
+    fontSize: '15px',
+    width: '100%',
+  },
+  passwordContainer: {
+    display: 'flex',
+    width: '110%',
+  },
+  passwordInput: {
+    flex: 1,
+    padding: '12px',
+    borderRadius: '8px 0 0 8px',
+    border: '1px solid #CBD5E1',
+    fontSize: '15px',
+  },
+  toggleButton: {
+    padding: '0 15px',
+    border: '1px solid #CBD5E1',
+    borderLeft: 'none',
+    backgroundColor: '#E2E8F0',
+    cursor: 'pointer',
+    borderRadius: '0 8px 8px 0',
+  },
+  submitButton: {
+    background: 'linear-gradient(90deg, #467fc1, #467fc1)',
+    color: '#fff',
+    padding: '12px',
+    border: 'none',
+    borderRadius: '8px',
+    fontSize: '16px',
+    cursor: 'pointer',
+  },
+  bottomText: {
+    marginTop: '15px',
+    fontSize: '14px',
+  },
+};
+
 export default LoginForm;
